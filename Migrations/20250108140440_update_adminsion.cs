7b2409b9bc9a4fc25684dc0159f0953c973699b8
doanhuyxh <doanhuyxh@gmail.com>
2025-01-09 22:28:02 +0700

    up

---
+++ Migrations/20250108140440_update_adminsion.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BeApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class update_adminsion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Class",
+                table: "Admission");
+
+            migrationBuilder.DropColumn(
+                name: "IsPayment",
+                table: "Admission");
+
+            migrationBuilder.DropColumn(
+                name: "StudentCode",
+                table: "Admission");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Class",
+                table: "Admission",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPayment",
+                table: "Admission",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "StudentCode",
+                table: "Admission",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
---
+++ Migrations/20250109001923_update-db.cs
@@ -0,0 +1,141 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BeApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class updatedb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_StudentProfile_Faculty_FacultyId",
+                table: "StudentProfile");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_StudentProfile_Major_MajorId",
+                table: "StudentProfile");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_StudentProfile_StudentClass_StudentClassId",
+                table: "StudentProfile");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "StudentClassId",
+                table: "StudentProfile",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "MajorId",
+                table: "StudentProfile",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "FacultyId",
+                table: "StudentProfile",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_StudentProfile_Faculty_FacultyId",
+                table: "StudentProfile",
+                column: "FacultyId",
+                principalTable: "Faculty",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_StudentProfile_Major_MajorId",
+                table: "StudentProfile",
+                column: "MajorId",
+                principalTable: "Major",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_StudentProfile_StudentClass_StudentClassId",
+                table: "StudentProfile",
+                column: "StudentClassId",
+                principalTable: "StudentClass",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_StudentProfile_Faculty_FacultyId",
+                table: "StudentProfile");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_StudentProfile_Major_MajorId",
+                table: "StudentProfile");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_StudentProfile_StudentClass_StudentClassId",
+                table: "StudentProfile");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "StudentClassId",
+                table: "StudentProfile",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "MajorId",
+                table: "StudentProfile",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "FacultyId",
+                table: "StudentProfile",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_StudentProfile_Faculty_FacultyId",
+                table: "StudentProfile",
+                column: "FacultyId",
+                principalTable: "Faculty",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_StudentProfile_Major_MajorId",
+                table: "StudentProfile",
+                column: "MajorId",
+                principalTable: "Major",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_StudentProfile_StudentClass_StudentClassId",
+                table: "StudentProfile",
+                column: "StudentClassId",
+                principalTable: "StudentClass",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
